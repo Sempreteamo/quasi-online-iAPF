2a0c4dab3d687c0e01f9c6a0d695299ae7f97b46
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-03-18 23:58:54 +0000

    Create run_iAPF.R

---
+++ run_iAPF.R
@@ -0,0 +1,15 @@
+run_iAPF <- function(n, w, X, L, N, Time, d){
+  output <- init_APF(n, w, X, L)
+  X_apf <- output[[1]]
+  w_apf <- output[[2]]
+  Z_apf <- output[[3]]
+  
+  output2 <- psi_APF(n, X_apf, Z_apf, w, X, L, N, Time, d)
+  
+  #smoothing particles
+  X <- output2[[1]]
+  w <- output2[[2]]
+  psi <- output2[[3]]
+
+   return(list(X, w, psi))
+}
