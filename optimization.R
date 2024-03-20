babf6e8c30eda8a3b13dbb15a2f52a85e7692eb4
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-03-20 23:55:29 +0000

    Update optimization.R

--- optimization.R
+++ optimization.R
@@ -1,9 +1,11 @@
 optimization <- function(x, psi, d){
+  psi_pa1 <- vector()
+  
   coef <- -lm(log(psi[t,])~., data.frame(x^2, x))$coefficients
   a <- coef[2:(1+d)]
   b <- coef[(2+d):length(coef)]
   
-  psi_pa1[t,] <- c(solve(-2 * diag(a)) %*% b, diag(solve(2 * diag(a))))
+  psi_pa1 <- c(b/(-2*a), 1/(2*a))
   
   return(psi_pa1)
 }
