f85295f971bf6bece95ce17977d029da7210443b
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-03-22 01:17:45 +0000

    Update psi.R

--- psi.R
+++ psi.R
@@ -1,25 +1,29 @@
-Psi <- function(l, n, X_apf, N, L, Time){
-  psi <- matrix(NA, nrow = Time, ncol = N[l])
+#N=N[l], x = X_apf[t,,]
+
+Psi <- function(n, x, N, L, Time, d, obs){
+  psi <- matrix(NA, nrow = Time, ncol = N)
   psi_pa1 <- matrix(NA, nrow = Time, ncol = 2*d)
   
   #calculate psi
   for(t in n:(n-L+1)){
     if(t == n){
-      for(i in 1:N[l]){
+      for(i in 1:N){
         psi[t,i] <- (1 / ((2 * pi)^(d / 2))) * 
-          exp(-0.5 * t(X_apf[t,i,] - obs[t,]) %*% (X_apf[t,i,] - obs[t,]))
+          exp(-0.5 * t(x[i] - obs[t,]) %*% (x[i] - obs[t,]))
       }
       
       
     }else{
-      for(i in 1:N[l]){
+      for(i in 1:N){
         
-        psi[t,i] <- exp(g(obs[t,],X_apf[t,i,]))*dmvn(as.vector(A%*%X_apf[t,i,]), 
-                                                     psi_pa1[t+1, 1:d], diag(psi_pa1[t+1, (d+1):(d+d)]+1, nrow=d,ncol=d))
-      }
+        psi[t,i] <- exp(g(obs[t,],x[i]))*(2*pi)^(-d/2)*prod(psi_pa1[t+1, (d+1):(d+d)]+1)^(-1/2)*
+          exp(-(1/2)*t(A%*%x[i] - psi_pa1[t+1, 1:d])%*%diag((psi_pa1[t+1, (d+1):(d+d)]+B)^(-1), nrow=d,ncol=d)%*%
+                (A%*%x[i] - psi_pa1[t+1, 1:d]) )
+    
+        }
     }
     
-    psi_pa1 <- optimization(X_apf, t, psi, d)
+    psi_pa1[t,] <- optimization(x, psi, d)
     
     
     print(psi_pa1[t, ])
