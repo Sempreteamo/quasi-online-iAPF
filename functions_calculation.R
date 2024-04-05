eb341c3425f26b5dc6cf0d1720c424287b7869f9
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-04-05 17:34:01 +0100

    Update functions_calculation.R

--- functions_calculation.R
+++ functions_calculation.R
@@ -1,33 +1,3 @@
-#ess resampling
-ESS <- function(t,w, is.log=FALSE){
-  if(is.log) {
-    mx <- max(w[t-1,])
-    s <- sum(exp(w[t-1,]-mx))
-    ess <- 1/sum((exp(w[t-1,]-mx)/s)^2)
-  }else{
-    s <- sum(w[t-1,])
-    ess <- 1/sum((w[t-1,]/s)^2) 
-  }
-  return(ess)  
-}
-
-residual <- function(t, w){
-  mx <- max(w[t-1,])
-  w_ <- exp(w[t-1,] - mx)/sum(exp(w[t-1,] - mx))
-  
-  Ntm <- as.integer(Num*w_)
-  
-  mix <- unlist(lapply(1:Num, function(i) {rep(i, Ntm[i])}))
-  mr <- Num - sum(Ntm)
-  
-  w_hat <- w_ - Ntm/Num
-  w_hat <- w_hat*Num/mr
-  
-  mix <- c(sample(1:Num, mr, replace = TRUE, prob = w_hat), mix)
-  
-  return(mix)
-  
-} 
 
 Num_apf <- function(Z, l, k){
   return(sd(exp(Z[max(l-k,1):l]-max(Z[max(l-k,1):l])))/mean(exp(Z[max(l-k,1):l]-max(Z[max(l-k,1):l]))))
@@ -36,20 +6,20 @@ Num_apf <- function(Z, l, k){
 #trans prob
 #N(; Ax, B)
 f <- function(x){
-  return (rnorm(d) + A%*%x)   #trans prob
+  return (rnorm(d, 0, diag(B)) + A%*%x)   #trans prob
 }
 
 #obs prob  
 #N(; Cx, D)
 g <- function(y, x){  
-  return (log((2*pi)^(-d/2)) + (-1/2)*t(y-x)%*%(y-x)) #obs prob  C%*%x = x
+  return ((-d/2)*log(2*pi) + (-1/2)*t(y-x)%*%(y-x)) #obs prob  C%*%x = x
 }
 
 #twisted mu
 mu_aux <- function(psi_pa, l, N, t){  
-  return(rmvn(N[l], diag(((psi_pa[t, (d+1):(d+d)])^(-1)+1)^(-1), nrow=d,ncol=d)%*%
-                (diag((psi_pa[t, (d+1):(d+d)])^(-1), nrow=d,ncol=d)%*%psi_pa[t,1:d]), 
-              diag(((psi_pa[t, (d+1):(d+d)])^(-1)+1)^(-1), nrow=d,ncol=d)))
+  return(rmvn(N[l], diag((psi_pa[t, (d+1):(d+d)]^(-1) + 1)^(-1), nrow = d, ncol = d)%*%
+                (diag(psi_pa[t, (d+1):(d+d)]^(-1), nrow = d, ncol = d)%*%psi_pa[t,1:d]), 
+                diag((psi_pa[t, (d+1):(d+d)]^(-1) + 1)^(-1), nrow=d, ncol = d)))
 }
 
 #twisted g
@@ -66,12 +36,8 @@ g_aux <- function(y, x, t, psi_pa, n, L){
 
 #t=n-L+1 in APF for n!=L
 g_transition <- function(y, x, t, psi_pa, n){
-  return(g(y, x) + psi_tilda(x, psi_pa, t, n) -
-           psi_t(x, psi_pa, t, n))
+  return(g(y, x) + psi_tilda(x, psi_pa, t, n) - psi_t(x, psi_pa, t, n))
 }
-#(1/2)*t(-psi_pa[t, 1:d])%*%
-# diag((psi_pa[t, (d+1):(d+d)]+1)^(-1), nrow=d,ncol=d)%*%
-#(-psi_pa[t, 1:d])
 
 g_aux_smoo <- function(y, x, t, psi_pa, n){  
   if(t == 1){
@@ -86,9 +52,9 @@ g_aux_smoo <- function(y, x, t, psi_pa, n){
 
 #twisted f
 f_aux <- function(x, psi_pa, t){
-  return(rmvn(1, diag(((psi_pa[t, (d+1):(d+d)])^(-1)+1)^(-1), nrow=d,ncol=d)%*%
-                (A%*%x + diag(psi_pa[t, (d+1):(d+d)]^(-1), nrow=d,ncol=d)%*%psi_pa[t,1:d]), 
-              diag(((psi_pa[t, (d+1):(d+d)])^(-1)+1)^(-1), nrow=d,ncol=d)))  #f_2:T 
+  return(diag(((psi_pa[t, (d+1):(d+d)])^(-1)+1)^(-1), nrow=d,ncol=d)%*%
+            (A%*%x + diag(psi_pa[t, (d+1):(d+d)]^(-1), nrow=d,ncol=d)%*%psi_pa[t,1:d]) +
+            rnorm(d, 0, ((psi_pa[t, (d+1):(d+d)])^(-1)+1)^(-1)))  #f_2:T 
 }
 
 #psi.tilda_t = f (xt, ψt+1); psi.tilda_n = 1
@@ -115,21 +81,19 @@ psi_t <- function(x, psi_pa, t, n){
   return(psi_t)
 }
 
-#the distribution here and the distribution below should be double checked
-
 #the initial distribution mu at time kL
 # ∑W_kL(?)^i*f(X_{k−1}^i,(k−1)L, ·), i in 1:N
-#I used the weights at time (k-1)L because these are the closest weights, 
 #sample particles X_{kL-L+1} using the particles at time X_(k-1)L
 
-change_mu <- function(n, w, X, L){
-  sam <- matrix(0,Num,d)
-  mx <- max(w[n-L,])
-  w_ <- exp(w[n-L,]-mx)/sum(exp(w[n-L,] - mx))
+#x = w[n-L,]
+#mus = X[n-L,,]
+change_mu <- function(x, mus){
+  sam <- matrix(0, Num, d)
+  mx <- max(x)
+  w_ <- exp(x - mx)/sum(exp(x - mx))
   s <- sample(1:Num, size = Num, replace = TRUE, prob = w_) 
-  mus <- X[n-L,,]
   for(i in 1:Num){
-    sam[i,] <- rnorm(d) + A%*%mus[s[i],]
+    sam[i,] <- rnorm(d, 0, diag(B)) + A%*%mus[s[i],]
   }
   return(list(sam, w_))
 }
@@ -137,38 +101,38 @@ change_mu <- function(n, w, X, L){
 #the initial distribution mu.tilda at time kL
 #the particles and weights I use are exactly the same as above, and I use
 #f.tilda to generate particles instead of f
-change_mupsi <- function(n, w, X, psi_pa, t, N, l, L){
-  sam <- matrix(0,Num,d)
+
+#x = w[n-L,]
+#mus = X[n-L,,]
+change_mupsi <- function(mus, x, psi_pa, t, N, l){
+  sam <- matrix(0, Num, d)
   w_adj <- vector()
-  mx <- max(w[n-L,])
-  w_ <- exp(w[n-L,]-mx)/sum(exp(w[n-L,] - mx)) #normalized weights of w[n-L,]
+  mx <- max(x)
+  w_ <- exp(x - mx)/sum(exp(x - mx)) #normalized weights of w[n-L,]
   #here we need to adjust the weights 
   
   for(i in 1:Num){
-    w_adj[i] <- w[n-L,i] + (-1/2)*t(A%*%X[n-L,i,] - psi_pa[t,1:d])%*%diag((psi_pa[t, (d+1):(d+d)]+1)^(-1), nrow=d,ncol=d)%*%
-      (A%*%X[n-L,i,] - psi_pa[t,1:d])
+    w_adj[i] <- x[i] + (-1/2)*t(A%*%mus[i,] - psi_pa[t,1:d])%*%diag((psi_pa[t, (d+1):(d+d)]+1)^(-1), nrow=d,ncol=d)%*%
+      (A%*%mus[i,] - psi_pa[t,1:d])
   }
   
   mx <- max(w_adj)
-  w_tilda <- exp(w_adj-mx)/sum(exp(w_adj - mx)) #adjusted normalized weights
+  w_tilda <- exp(w_adj - mx)/sum(exp(w_adj - mx)) #adjusted normalized weights
   s <- sample(1:Num, size = Num, replace = TRUE, prob = w_tilda) 
-  mus <- X[n-L,,]
   
   #use the adjusted normalized weights to generate particles
   for(i in 1:N[l]){
-    sam[i,] <- rmvn(1,  diag((psi_pa[t, (d+1):(d+d)]^(-1)+1)^(-1), nrow=d,ncol=d)%*%
-                      (diag(psi_pa[t, (d+1):(d+d)]^(-1), nrow=d,ncol=d)%*%psi_pa[t,1:d] + A%*%mus[s[i],]),
-                    diag((psi_pa[t, (d+1):(d+d)]^(-1)+1)^(-1), nrow=d,ncol=d))
+    sam[i,] <- diag((psi_pa[t, (d+1):(d+d)]^(-1)+1)^(-1), nrow=d,ncol=d)%*%
+      (diag(psi_pa[t, (d+1):(d+d)]^(-1), nrow=d,ncol=d)%*%psi_pa[t,1:d] + A%*%mus[s[i],]) + 
+      rnorm(d, 0, (psi_pa[t, (d+1):(d+d)]^(-1)+1)^(-1))
   }
-  
   
   #calculate the normalizing constant
   sum_ <- 0
   for(i in 1:N[l]){
     sum_ = sum_ + exp(w_[i])*
-      exp((-1/2)*t(A%*%X[n-L,i,] - psi_pa[t,1:d])%*%diag((psi_pa[t, (d+1):(d+d)]+1)^(-1), nrow=d,ncol=d)%*%
-            (A%*%X[n-L,i,] - psi_pa[t,1:d]))
-    
+      exp((-1/2)*t(A%*%mus[i,] - psi_pa[t,1:d])%*%diag((psi_pa[t, (d+1):(d+d)]+1)^(-1), nrow=d,ncol=d)%*%
+            (A%*%mus[i,] - psi_pa[t,1:d]))
   }
   #psi_tilda_0 = sum(w_i*z_c^i)
   sum_ <- (2*pi)^(-1/2)*(det(diag(psi_pa[t, (d+1):(d+d)]+1, nrow=d,ncol=d)))^(-1/2)*sum_
