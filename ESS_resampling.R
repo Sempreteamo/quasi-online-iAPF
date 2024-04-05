dd08b045d9c331cf88e4190d33de5db5235b5e28
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-04-05 12:46:32 +0100

    Create ESS_resampling.R

---
+++ ESS_resampling.R
@@ -0,0 +1,15 @@
+#ess resampling
+
+#x = w[t-1,]
+
+ESS <- function(x, t,w, is.log=FALSE){
+  if(is.log) {
+    mx <- max(x)
+    s <- sum(exp(x - mx))
+    ess <- 1/sum((exp(x - mx)/s)^2)
+  }else{
+    s <- sum(x)
+    ess <- 1/sum((x/s)^2) 
+  }
+  return(ess)  
+}
