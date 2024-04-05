dabd78bd33912889436d5e64e3d76afaf65c789a
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-04-05 17:54:51 +0100

    Update observation.R

--- observation.R
+++ observation.R
@@ -1,10 +1,10 @@
-Obs <- function(d, Time,){
+Obs <- function(d, Time){
   #set.seed(123)
-  X_true[1,] <- rnorm(d)     
+  X_true[1,] <- rnorm(d, 0, diag(B))     
   for(t in 2:Time){ 
     #set.seed(123)#observations
-    X_true[t,] <- rnorm(d) + A%*%X_true[t-1,]  #t(rmvn(d) + A%*%x)
+    X_true[t,] <- rnorm(d, 0, diag(B)) + A%*%X_true[t-1,]  #t(rmvn(d) + A%*%x)
   }
   #set.seed(123)
-  return(matrix(rnorm(Time*d, X_true, 1), ncol = d))
+  return(matrix(rnorm(Time*d, c%*%X_true, D), ncol = d))
 }
