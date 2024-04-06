184a39e7657d56c43f511e2af6901a5057ea99fc
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-04-06 17:21:28 +0100

    Update functions_calculation.R

--- functions_calculation.R
+++ functions_calculation.R
@@ -23,7 +23,7 @@ mu_aux <- function(psi_pa, l, N, t){
 }
 
 #twisted g
-g_aux <- function(y, x, t, psi_pa, n, L){  
+g_aux <- function(y, x, t, psi_pa, n){  
   if(t == (n-L+1)){
     return(g(y, x) + psi_tilda(x, psi_pa, t, n) -(1/2)*log(2*pi)-(1/2)*log(det(diag(psi_pa[t, (d+1):(d+d)]+1, nrow=d,ncol=d))) - 
              (1/2)*t(-psi_pa[t, 1:d])%*%
