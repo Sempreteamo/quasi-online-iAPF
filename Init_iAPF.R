94d99aa1fa0f89d4195d27c34ed6ca486a186937
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-04-16 10:25:59 +0100

    Update and rename Init_of_iAPF.R to Init_iAPF.R

--- Init_iAPF.R
+++ Init_iAPF.R
@@ -1,37 +1,37 @@
-
-
 #Below is the iAPF algorithm
 #First we do the initialization
 ####init_APF####
-init_APF <- function(n, w, X, L){  #pure filtering
-  l = 1
-  Z_apf[1] = 0
+init_APF <- function(n, w, X, L, N){  #pure filtering
+  Z_apf = 0
   X_init <- array(NA, dim = c(Time, Num, d))
-  w_init <- matrix(NA, Time, N[l])
+  w_init <- matrix(NA, Time, N)
   
   #when n = L, we use the mu as the initialization distribution mu; 
   #when n = kL, we use the new distribution
   #I tried to modify the previous paths from 1 to n-L+1
-  #X_init[1:(n-L+1),,] <- rnorm(N[l]*d)  
-  #for(i in 1:N[l]){
+  #X_init[1:(n-L+1),,] <- rnorm(N*d)  
+  #for(i in 1:N){
   # w_init[1:(n-L+1),i] <- g(obs[n-L+1,], X_init[n-L+1,i,])  
   #}
   
   if(n == L){
-    X_init[n-L+1,,] <- rnorm(N[l]*d)  
+    X_init[n-L+1,,] <- rnorm(N*d)  
     for (i in 1:(n-L)) {
       X_init[i,,] <- X_init[n-L+1,,]
     }
-    for(i in 1:N[l]){
+    for(i in 1:N){
       w_init[1:(n-L+1),i] <- g(obs[n-L+1,], X_init[n-L+1,i,])  
     }
   }else{
+    cat('n=',n,'L=',L)
+    cat('w[n-L]=', w[n-L,])
     output <- change_mu(w[n-L,], X[n-L,,])
     X_init[n-L+1,,] <- output[[1]]
     for (i in 1:(n-L)) {
       X_init[i,,] <- X_init[n-L+1,,]
     }
     w_ <- output[[2]]
+    print('pass')
     
     
     for (i in 1:Num){
@@ -43,28 +43,28 @@ init_APF <- function(n, w, X, L){  #pure filtering
   
   for(t in (n-L+2):n){
     
-    if(ESS(w_init[t-1,], is.log=TRUE) <= kappa*N[l]){
+    if(ESS(w_init[t-1,], is.log=TRUE) <= kappa*N){
       
-      output <- residual_resampling(w_init[t-1,], Num, Z_apf[l])
+      output <- residual_resampling(w_init[t-1,], Z_apf)
       mix <- output[[1]]
       Z_apf <- output[[2]]
       # at the initialization stage, we want filtering particles for psi
       
-      for(i in 1:N[l]){
+      for(i in 1:N){
         X_init[t,i,] <- f(X_init[t-1,mix[i],]) 
         w_init[t,i] <- g(obs[t,], X_init[t,i,])  
       }
       
     }else{
-      for(i in 1:N[l]){
+      for(i in 1:N){
         X_init[t,i,] <- f(X_init[t-1,i,]) 
         w_init[t,i] <- w_init[t-1,i] + g(obs[t,], X_init[t,i,])  
       }
     }
   }
   
-  mx <- max(w_init[n, 1:N[l]])
-  Z_apf[1] <- Z_apf[1] + log(mean(exp(w_init[n, 1:N[l]]-mx))) + mx
+  mx <- max(w_init[n, 1:N])
+  Z_apf <- Z_apf + log(mean(exp(w_init[n, 1:N]-mx))) + mx
   return(list(X_init = X_init, w_init = w_init, Z_apf))
 }
-  
+
