bfcd2cb126e98f59988f827e426e2d0a5eae2040
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-04-16 10:28:08 +0100

    Update and rename main_part_of_iAPF.R to iterate_psi_APF.R

--- iterate_psi_APF.R
+++ iterate_psi_APF.R
@@ -1,5 +1,4 @@
-
-psi_APF <- function(n, X_apf, Z_apf, w, X, L, N, Time, d, obs){
+iterate_psi_APF <- function(n, X_apf, Z_apf, w, X, L, N){
   l = 1
   
   while(TRUE){
@@ -7,24 +6,24 @@ psi_APF <- function(n, X_apf, Z_apf, w, X, L, N, Time, d, obs){
     output <- list()
     
     if(l != 1){
-      print(l)
+      #print(l)
       #generate filtering particles X_apf for psi the next iteration
       #APF outputs filtering X_apf for the next psi, and smoothing X_apf_s
       #for the final calculation
       
-      output <- APF(Time, d, n, w, X, psi_pa, l, Z_apf, N, L)
+      output <- psi_APF(n, w, X, psi_pa, N[l], L)
       X_apf <- output[[1]]
       w_apf <- output[[2]]
-      Z_apf <- output[[3]]
+      Z_apf[l] <- output[[3]]
     }
     
     #to speed up the algorithm, I just fix the number of iterations to be k.
     #Here k = 5
     
     if(l <= k ){
-      
+      print(l)
       #receive filtering particles X_apf for psi
-      psi_pa <- Psi(n, X_apf[t,,], N, L, Time, d, obs) 
+      psi_pa <- Psi(n, X_apf, N[l], L) 
       
       if(l > k & N[max(l-k,1)] == N[l] & is.unsorted(Z_apf[max(l-k,1):l])){  
         N[l+1] <- 2*N[l]
