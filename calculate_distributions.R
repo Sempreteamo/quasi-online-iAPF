5fea60ada429e22b3fa7dbadbb6aea8be6ef4202
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-04-22 18:18:08 +0100

    Update calculate_distributions.R

--- calculate_distributions.R
+++ calculate_distributions.R
@@ -61,8 +61,8 @@ psi_tilda <- function(x, psi_pa, t, n){  #from 0 to T. 0,T = 1
   if(t == n){
     psi_tilda <- 0
   }else{   #psi_pa_t = psi_t
-    psi_tilda <- (-1/2)*log(2*pi) -(1/2)*log(det(diag(psi_pa[t+1, (d+1):(d+d)]+1, nrow=d, ncol=d))) +
-      (-1/2)*t(A%*%x - psi_pa[t+1, 1:d])%*%diag((psi_pa[t+1, (d+1):(d+d)]+1)^(-1), nrow=d,ncol=d)%*%
+    psi_tilda <- (-1/2)*log(2*pi) -(1/2)*log(det(diag(psi_pa[t+1, (d+1):(d+d)]+ diag(B), nrow=d, ncol=d))) +
+      (-1/2)*t(A%*%x - psi_pa[t+1, 1:d])%*%diag((psi_pa[t+1, (d+1):(d+d)] + diag(B))^(-1), nrow=d,ncol=d)%*%
       (A%*%x-psi_pa[t+1, 1:d]) 
   }
   return(psi_tilda)
